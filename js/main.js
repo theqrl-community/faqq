bd9b8807dee81c0e13cdf242857088dda19be022
Jack Matier <self@jackalyst.com>
2019-04-23 21:30:12 -0600

    Cleanup & 1 less request

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-// Not going to need jQuery for this...
-// function updatehash() {
-//     var hash = window.location.hash.substring(1);
-
-//     if(document.querySelector('.view')) {
-//     	document.querySelector('.view').classList.remove('view');
-// 	}
-    
-//     document.getElementById(hash).classList.add('view');
-// }
-
-// window.addEventListener("hashchange", updatehash);
-
-// if(!window.location.hash) {
-// 	window.location.hash = "#quantum-computing-is-coming";
-	
-// }
-// updatehash();
-
-
-
 setTimeout(function() {
 	document.querySelector('.section').classList.add('view');
 },1);
-
